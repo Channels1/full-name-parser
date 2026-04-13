f97853c86627a92c54f6d26f0f9270c1b3bdbd58
jihaddrake <ringod40@gmail.com>
2026-04-13 15:04:46 -0400

    Added in string array

--- src/main/java/com/pluralsight/fullNameParseApp.java
+++ src/main/java/com/pluralsight/fullNameParseApp.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class fullNameParseApp {
+    public static void main(String[] args) {
+        Scanner logger = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String fullName = logger.nextLine();
+
+        String[] data = fullName.split(" ");
+        System.out.println("First name:"+data[0]);
+        System.out.println("Middle name:"+data[1]);
+        System.out.println("Last name:"+data[2]);
+
+    }
 }
